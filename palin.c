b40b328bac8aead90270f45434970aa84f441480
Jegadheep <Jegadheep@gmail.com>
2016-12-29 17:29:35 +0530

    Update palin.c

--- palin.c
+++ palin.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-viod main()
+void main()
 {
-	int n,m,rev=0,r;
+  int n,m,rev=0,r;
   printf("enter the number");
   scanf("%d",&n);
   m=n;
-	while(n>0)
+  while(n>0)
   {
-		r=n%10;
-		n=n/10;
-		rev=rev*10+r;
-	}
+     r=n%10;
+     n=n/10;
+     rev=rev*10+r;
+  }
   if(rev==m)
   {
-	printf("the number is a palindrome");
+     printf("the number is a palindrome");
   }
   else
   {
-  printf("the number is not a palindrome");
+     printf("the number is not a palindrome");
   }
 }
