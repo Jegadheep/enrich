3486fe2316ceaae39da0c06e70bac1bb7c8171bf
Jegadheep <Jegadheep@gmail.com>
2016-12-31 13:50:41 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    
+	int n,i,j,c=0;
+	printf("enter the length");
+	scanf("%d",&n);
+	printf("\nenter the elements");
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	printf("\nenter the number");
+	scanf("%d",&j);
+	for(i=0;a[i]<=j;i++)
+	{
+		if(j==a[i])
+		{
+			c++;
+		}
+	}
+	printf("\n%d",c);
+	return 0;
+}
